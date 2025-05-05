0e143d50756131b14ea967b150281c4aaf6393c9
Dioni Alves <dionialves@gmail.com>
2025-05-04 22:41:37 -0300

    Adjusted some css settings

---
+++ src/components/Index.jsx
@@ -0,0 +1,10 @@
+
+function Index() {
+    return (
+        <div class="index">
+            <h1>Clique <a href="http://sip.getel.net.br/circuits">AQUI</a> para acessar os circuitos</h1>}
+        </div>
+    );
+}
+
+export default Index;
