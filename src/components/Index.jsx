de1c932d8982e9e5eb06b4d94bcef93b05c6a731
Dioni Alves <dionialves@gmail.com>
2025-05-20 14:18:41 -0300

    Adjusted url from website

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,11 +1,10 @@
-const urlSite = import.meta.env.VITE_URL_SITE;
 
 function Index() {
     return (
         <div className="app-container-index">
             <div className="index">
                 <h1>
-                    Clique <a href={`${urlSite}/circuits`}>AQUI</a> para acessar os circuitos
+                    Clique <a href={`/circuits`}>AQUI</a> para acessar os circuitos
                 </h1>
             </div>
         </div>
